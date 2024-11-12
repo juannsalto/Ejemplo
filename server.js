9b8a12898e2d6892bc222daf4c69f25844b83e03
juannsalto <115878705+juannsalto@users.noreply.github.com>
2024-11-12 19:12:46 -0300

    ruta inicio

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Middleware para servir archivos estáticos (opcional)
-app.use(express.static('public'));
 
 // Ruta de ejemplo
 app.get('/', (req, res) => {
   res.send('¡Hola Mundo desde Express!');
+});
+
+// Ruta para servir el archivo logueo.html
+app.get('/inicio', (req, res) => {
+  res.sendFile(path.join(__dirname,'logueo.html'));  // Asegúrate de que 'logueo.html' esté dentro de 'public'
 });
 
 // Iniciar el servidor
